c864758299239f78a9260195238869845ca7c1b4
Gustavo <gufoxy194@gmail.com>
2023-08-01 07:22:07 -0300

    fizemos o sistema de editar e excluir

--- controller.js
+++ controller.js
@@ -15,6 +15,34 @@ function salvarUser(){
     let nomeUser = document.getElementById("nomeUser").value;
     
     if(nomeUser){
-        dadosLista.push(nomeUser)
+        dadosLista.push(nomeUser);
+        criarLista();
+        document.getElementById('nomeUser').value = '';
+    
     }
+}
+
+//função para criar linhas de usuario
+
+function criarLista(){
+    let tabela = document.getElementById('tabela').innerHTML =  '<tr><th>Nome Usuario</th><th>Ações</th></tr>';
+
+    for(let i = 0;i <= (dadosLista.length - 1);i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class ='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>editar</button><button class ='btn btn-danger'onclick='excluir(this.parentNode.parentNode.rowIndex)'>excluir</button></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
+
+// função editar nome
+
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i-1)], 1);
+}
+
+// função excluir nome
+
+function excluir(i){
+    dadosLista.splice((i-1),1);
+    document.getElementById('tabela').deleteRow(i);
 }
